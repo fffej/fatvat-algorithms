9672bc4305de2bb4b6553f19c350805b97f394b6
Jeff Foster <jfoster@acm.org>
2010-12-11 09:48:22 +0000

    and now it works relatively well

--- word-ladders/haskell/WordLadders.hs
+++ word-ladders/haskell/WordLadders.hs
@@ -33,7 +33,7 @@ search graph maxDepth goal = search' graph maxDepth goal []
 search' :: Node -> Int -> String -> [String] -> [String]
 search' (Node end children) maxDepth goal path 
   | end == goal    = end : path -- finished
-  | null children  = [] -- no where left to go
+  | null children  = [] -- no where left to goal
   | length path >= maxDepth = [] -- too deep
   | (difference end goal) >= maxDepth - length path = [] -- too much difference
   | otherwise = quickest
@@ -41,7 +41,7 @@ search' (Node end children) maxDepth goal path
       -- search all the children
       childRoutes = filter (not . null) $ map (\child -> search' child maxDepth goal (end : path)) children
       quickest | null childRoutes = []
-               | otherwise        = minimumBy (comparing length) (filter (not . null) childRoutes)
+               | otherwise        = head childRoutes                                    
                                             
 -- Two strings are a neighbour if they differ by only a single character
 neighbour :: String -> String -> Bool
